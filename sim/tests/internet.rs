90540453adb56412392ec828d32a3602a13d52af
Tim Harding <tim@timharding.co>
2022-07-03 18:45:56 -0700

    Added Ipv4 to internet test

--- sim/tests/internet.rs
+++ sim/tests/internet.rs
@@ -1,9 +1,8 @@
 use elvis::{
     applications::{Capture, SendMessage},
     core::{Internet, InternetError, Machine, Message, Network, RcProtocol},
-    protocols::Tap,
+    protocols::{Tap, Ipv4},
 };
-use std::iter;
 
 // Todo: Test that the message is actually received
 // Todo: Test both send and receive
@@ -13,12 +12,16 @@ pub fn internet() -> Result<(), InternetError> {
     let network = Network::new(vec![0, 1], 1500);
 
     let sender_tap = Tap::new_shared(vec![network.mtu()]);
+    let sender_ip = Ipv4::new_shared();
     let send_message = SendMessage::new_shared("Hello!");
-    let sender_machine = Machine::new(sender_tap, iter::once(send_message as RcProtocol))?;
+    let sender_protocols: [RcProtocol; 2] = [sender_ip, send_message];
+    let sender_machine = Machine::new(sender_tap, sender_protocols.into_iter())?;
 
     let receiver_tap = Tap::new_shared(vec![network.mtu()]);
+    let receiver_ip = Ipv4::new_shared();
     let capture = Capture::new_shared();
-    let receiver_machine = Machine::new(receiver_tap, iter::once(capture.clone() as RcProtocol))?;
+    let receiver_protocols: [RcProtocol; 2] = [receiver_ip, capture.clone()];
+    let receiver_machine = Machine::new(receiver_tap, receiver_protocols.into_iter())?;
 
     let mut internet = Internet::new(vec![receiver_machine, sender_machine], vec![network]);
     internet.run()?;
