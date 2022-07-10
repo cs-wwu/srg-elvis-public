22865abe12d3166431447a61c09dde1b69d05546
Tim Harding <tim@timharding.co>
2022-07-10 14:30:55 -0700

    Documentation for Session

--- sim/src/core/session.rs
+++ sim/src/core/session.rs
@@ -1,15 +1,31 @@
 use super::{Message, ProtocolContext, ProtocolId};
 use std::{cell::RefCell, error::Error, rc::Rc};
 
+/// A shared, mutable, reference to a [`Session`]
 pub type RcSession = Rc<RefCell<dyn Session>>;
 
-/// A Session holds state for a particular connection. A Session "belongs"
-/// to a Protocol.
+/// Holds the state for a particular connection.
+///
+/// A [`Protocol`](super::Protocol) creates a session to respond to a particular
+/// connection. For example, an IP protocol might create a session to handle
+/// messages with a particular pair of local and remote addresses. A session is
+/// in charge of appending headers to outgoing messages, deciding which protocol
+/// to use for demuxing incoming messages, and keeping track of state such as
+/// TCP windows.
 pub trait Session {
-    // Returns the ID of the protocol used to demux messages upward
+    /// Returns the ID of the protocol that creates and manages this session.
     fn protocol(&self) -> ProtocolId;
 
-    /// Invoked from a Protocol to send a Message.
+    /// Takes the message, appends headers, and forwards it to the next session
+    /// in the chain for further processing.
+    ///
+    /// # Arguments
+    ///
+    /// - `self_handle`: A shared handle to the session. This is the same as
+    ///   `self`, except that it is wrapped in an [`Rc`] and a [`RefCell`].
+    /// - `message`: The [`Message`] to process.
+    /// - `context`: The [`ProtocolContext`] used to get information from the
+    ///   containing [`Machine`](crate::core::Network).
     fn send(
         &mut self,
         self_handle: RcSession,
@@ -17,18 +33,16 @@ pub trait Session {
         context: &mut ProtocolContext,
     ) -> Result<(), Box<dyn Error>>;
 
-    // Todo: We probably want demux to have already parsed the header and then pass
-    // it on to the session. One of the things the x-kernel paper mentions is that
-    // we want to touch the header as few times as possible for best performance. At
-    // the moment, we require that the demux and recv methods each parse the header
-    // separately, which is evidently inefficient. Without being able to make
-    // Session generic, this does raise the question of what type would be
-    // appropriate for passing structured header information from one method to
-    // another. Do we possibly just attach information to the context? I'm not sure
-    // just how efficient getting values from a HashMap is compared to just parsing
-    // the header again. It's not entirely clear what to do here.
-
-    /// Invoked from a Protocol or Session object below for Message receipt.
+    /// Takes an incoming message and decides which protocol to send it to for
+    /// further processing.
+    ///
+    /// # Arguments
+    ///
+    /// - `self_handle`: A shared handle to the session. This is the same as
+    ///   `self`, except that it is wrapped in an [`Rc`] and a [`RefCell`].
+    /// - `message`: The [`Message`] to process.
+    /// - `context`: The [`ProtocolContext`] used to get information from the
+    ///   containing [`Machine`](crate::core::Network).
     fn recv(
         &mut self,
         self_handle: RcSession,
@@ -36,6 +50,19 @@ pub trait Session {
         context: &mut ProtocolContext,
     ) -> Result<(), Box<dyn Error>>;
 
+    /// Called to allow a session to carry out some work outside the context of
+    /// responding to a message.
+    ///
+    /// As an example, TCP may decide to retransmit packets or poll empty window
+    /// sizes even when no new messages are being sent or received. This
+    /// lifecycle method is a session's opportunity to carry out such tasks.
+    ///
+    /// # Arguments
+    ///
+    /// - `self_handle`: A shared handle to the session. This is the same as
+    ///   `self`, except that it is wrapped in an [`Rc`] and a [`RefCell`].
+    /// - `context`: The [`ProtocolContext`] used to get information from the
+    ///   containing [`Machine`](crate::core::Network).
     fn awake(
         &mut self,
         self_handle: RcSession,
@@ -43,7 +70,10 @@ pub trait Session {
     ) -> Result<(), Box<dyn Error>>;
 }
 
+/// Expresses what to do after a protocol is called on to run.
 pub enum ControlFlow {
+    /// Keep running the simulation
     Continue,
+    /// Stop running the simulation
     EndSimulation,
 }
