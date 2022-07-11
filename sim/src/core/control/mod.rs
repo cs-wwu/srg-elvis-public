7821cf21248532bee2bbebe9b144dd57345b2d44
Tim Harding <tim@timharding.co>
2022-07-10 22:52:06 -0700

    Moved ControlValue with Control

--- sim/src/core/control/mod.rs
+++ sim/src/core/control/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use thiserror::Error as ThisError;
 
 mod primitive;
 pub use primitive::Primitive;
@@ -43,4 +44,52 @@ impl Control {
     pub fn get(&self, key: &'static str) -> Option<Primitive> {
         self.0.get(&key).cloned()
     }
+}
+
+pub trait Bounds = TryFrom<Primitive> + Into<Primitive> + Copy + std::fmt::Debug;
+
+#[derive(Debug, Clone, Copy)]
+pub struct ControlValue<T: Bounds, const K: &'static str>(T);
+
+impl<T: Bounds, const K: &'static str> TryFrom<&Control> for ControlValue<T, K> {
+    type Error = ControlValueError<<T as TryFrom<Primitive>>::Error>;
+
+    fn try_from(control: &Control) -> Result<Self, Self::Error> {
+        Ok(Self(T::try_from(
+            control.get(K).ok_or(ControlValueError::Missing(K))?,
+        )?))
+    }
+}
+
+impl<T: Bounds, const K: &'static str> From<T> for ControlValue<T, K> {
+    fn from(t: T) -> Self {
+        Self(t)
+    }
+}
+
+impl<T: Bounds, const K: &'static str> ControlValue<T, K> {
+    pub fn set(control: &mut Control, value: T) {
+        control.insert(K, value)
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T: Bounds, const K: &'static str> ControlValue<T, K>
+where
+    <T as TryFrom<Primitive>>::Error: std::fmt::Debug,
+{
+    pub fn get(control: &Control) -> T {
+        Self::try_from(control).unwrap().into_inner()
+    }
+}
+
+#[derive(Debug, ThisError)]
+pub enum ControlValueError<E> {
+    #[error("Missing control key")]
+    Missing(&'static str),
+    #[error("{0}")]
+    Invalid(#[from] E),
 }
--- sim/src/protocols/ipv4/ipv4_misc.rs
+++ sim/src/protocols/ipv4/ipv4_misc.rs
@@ -1,54 +1,6 @@
 use super::ipv4_address::Ipv4Address;
-use crate::core::{control::Primitive, Control};
+use crate::core::control::ControlValue;
 use thiserror::Error as ThisError;
-
-pub trait Bounds = TryFrom<Primitive> + Into<Primitive> + Copy + std::fmt::Debug;
-
-#[derive(Debug, Clone, Copy)]
-pub struct ControlValue<T: Bounds, const K: &'static str>(T);
-
-impl<T: Bounds, const K: &'static str> TryFrom<&Control> for ControlValue<T, K> {
-    type Error = ControlValueError<<T as TryFrom<Primitive>>::Error>;
-
-    fn try_from(control: &Control) -> Result<Self, Self::Error> {
-        Ok(Self(T::try_from(
-            control.get(K).ok_or(ControlValueError::Missing(K))?,
-        )?))
-    }
-}
-
-impl<T: Bounds, const K: &'static str> From<T> for ControlValue<T, K> {
-    fn from(t: T) -> Self {
-        Self(t)
-    }
-}
-
-impl<T: Bounds, const K: &'static str> ControlValue<T, K> {
-    pub fn set(control: &mut Control, value: T) {
-        control.insert(K, value)
-    }
-
-    pub fn into_inner(self) -> T {
-        self.0
-    }
-}
-
-impl<T: Bounds, const K: &'static str> ControlValue<T, K>
-where
-    <T as TryFrom<Primitive>>::Error: std::fmt::Debug,
-{
-    pub fn get(control: &Control) -> T {
-        Self::try_from(control).unwrap().into_inner()
-    }
-}
-
-#[derive(Debug, ThisError)]
-pub enum ControlValueError<E> {
-    #[error("Missing control key")]
-    Missing(&'static str),
-    #[error("{0}")]
-    Invalid(#[from] E),
-}
 
 pub type LocalAddress = ControlValue<Ipv4Address, "ipv4_local_address">;
 pub type RemoteAddress = ControlValue<Ipv4Address, "ipv4_remote_address">;
