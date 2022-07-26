a2326fb64c8142ee90f533ee563dd1977ca1d75b
Tim Harding <tim@timharding.co>
2022-07-25 17:36:15 -0700

    Internet::new() clarifying comments

--- sim/src/core/internet.rs
+++ sim/src/core/internet.rs
@@ -1,36 +1,54 @@
 use super::{message::Message, ControlFlow, Machine, MachineId, Network};
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
+type MachineIndex = usize;
+type NetworkIndex = usize;
+
+/// A shared, mutable handle to a network. We will be handing these out to
+/// multiple [`Machine`]s at a time.
+type SharedNetwork = Rc<RefCell<Network>>;
+/// A shared but immutable list of networks in the simulation. We won't be
+/// changing which networks there are, so [`RefCell`] is not needed.
+type SharedNetworks = Rc<Vec<SharedNetwork>>;
+/// A shared handle to a list of network indices. These are used to track which
+/// networks are available to a given [`Machine`].
+type NetworkIndices = Rc<Vec<NetworkIndex>>;
+
 /// The top-level container that controls the simulation.
 pub struct Internet {
     machines: Vec<Machine>,
     /// Contains a mapping from a machine index to network indices
-    networks_for_machine: HashMap<usize, Rc<Vec<usize>>>,
-    networks: Rc<Vec<Rc<RefCell<Network>>>>,
+    networks_for_machine: HashMap<MachineIndex, NetworkIndices>,
+    networks: SharedNetworks,
 }
 
 impl Internet {
     /// Creates a new internet simulation with the given `machines` and
     /// `networks`
     pub fn new(machines: Vec<Machine>, networks: Vec<Network>) -> Self {
+        // Each network contain a list of which machines are attached to it. We
+        // also need the opposite, a list of which networks are accessible to
+        // each machine. We begin by looping over all machine indices.
         let networks_for_machine: HashMap<_, _> = (0..machines.len())
             .map(|machine_index| {
-                let networks: Vec<_> = networks
+                // We accumulate a list of which networks are reachable by this
+                // machine.
+                let networks_indices: Vec<_> = networks
                     .iter()
                     .enumerate()
                     .filter_map(|(network_index, network)| {
-                        if network
+                        // Check if the current machine index is one of the
+                        // network's connected machines. If so, include the
+                        // network in our list.
+                        network
                             .connected_machines()
                             .iter()
-                            .any(|&i| i == machine_index)
-                        {
-                            Some(network_index)
-                        } else {
-                            None
-                        }
+                            .any(|&connected| connected == machine_index)
+                            .then_some(network_index)
                     })
                     .collect();
-                (machine_index, Rc::new(networks))
+                // The key-value pair to store in the map
+                (machine_index, Rc::new(networks_indices))
             })
             .collect();
 
@@ -72,7 +90,7 @@ impl Internet {
 pub struct MachineContext {
     mac: MachineId,
     /// Contains a mapping from a machine index to network indices
-    networks_for_machine: Rc<Vec<usize>>,
+    networks_for_machine: Rc<Vec<NetworkIndex>>,
     networks: Rc<Vec<Rc<RefCell<Network>>>>,
 }
 
@@ -107,9 +125,9 @@ impl MachineContext {
 
 /// An iterator over networks neighboring the currently executing [`Machine`].
 struct NetworksIterator {
-    current: usize,
-    networks_for_machine: Rc<Vec<usize>>,
-    networks: Rc<Vec<Rc<RefCell<Network>>>>,
+    current: NetworkIndex,
+    networks_for_machine: NetworkIndices,
+    networks: SharedNetworks,
 }
 
 impl Iterator for NetworksIterator {
