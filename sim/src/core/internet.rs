1e980e4fc5c40d237f2a14e593cb5fa9a27a4ac6
Tim Harding <tim@timharding.co>
2022-08-05 11:20:01 -0700

    Removed the mutex from Internet

--- sim/src/core/internet.rs
+++ sim/src/core/internet.rs
@@ -1,13 +1,12 @@
 use tokio::sync::mpsc;
 
 use super::{Machine, Mtu, Network, NetworkId, SharedProtocol};
-use std::sync::{Arc, Mutex};
 
 /// The top-level container that controls the simulation.
 #[derive(Default)]
 pub struct Internet {
     machines: Vec<Machine>,
-    networks: Vec<Arc<Mutex<Network>>>,
+    networks: Vec<Network>,
 }
 
 impl Internet {
@@ -19,8 +18,7 @@ impl Internet {
     /// Adds a network to the simulation and returns a handle to it.
     pub fn network(&mut self, mtu: Mtu) -> NetworkId {
         let id: NetworkId = self.networks.len().try_into().unwrap();
-        self.networks
-            .push(Arc::new(Mutex::new(Network::new(id, mtu))));
+        self.networks.push(Network::new(id, mtu));
         id
     }
 
@@ -34,12 +32,9 @@ impl Internet {
         let mut machine = Machine::new(protocols, self.machines.len());
         for network in networks.into_iter() {
             self.networks
-                .get(network as usize)
+                .get_mut(network as usize)
                 .unwrap()
-                .clone()
-                .lock()
-                .unwrap()
-                .attach(&mut machine)
+                .attach(&mut machine);
         }
         self.machines.push(machine);
     }
@@ -47,8 +42,8 @@ impl Internet {
     /// Runs the simulation.
     pub async fn run(self) {
         let (shutdown_sender, mut shutdown_receiver) = mpsc::channel(1);
-        for network in self.networks {
-            network.lock().unwrap().start(shutdown_sender.clone());
+        for mut network in self.networks.into_iter() {
+            network.start(shutdown_sender.clone());
         }
         for mut machine in self.machines {
             machine.start(shutdown_sender.clone());
