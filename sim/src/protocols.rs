3392b7aaf868eb52f2e9df5421aa1a325565d1a7
Tim Harding <tim@timharding.co>
2022-08-11 19:23:35 -0700

    Protocol reexports

--- sim/src/protocols.rs
+++ sim/src/protocols.rs
@@ -1,7 +1,14 @@
-//! Fundatmental Internet protocols to be used by most simulations.
+//! Fundamental Internet protocols to be used by most simulations.
 
 pub mod ipv4;
+pub use ipv4::Ipv4;
+
 pub(crate) mod tap;
+
 pub mod udp;
+pub use udp::Udp;
+
 pub mod user_process;
+pub use user_process::UserProcess;
+
 mod utility;
