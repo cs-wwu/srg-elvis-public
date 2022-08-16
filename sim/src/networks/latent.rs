d014ff31e3814ad94a13391460cdce78e0f5aa8f
Tim Harding <tim@timharding.co>
2022-08-16 11:34:52 -0700

    Latent network todo impl for network

--- sim/src/networks/latent.rs
+++ sim/src/networks/latent.rs
@@ -1,1 +1,25 @@
+use crate::{
+    core::{network::Attachment, Network},
+    protocols::tap::Delivery,
+};
+use async_trait::async_trait;
+use std::{error::Error, sync::Arc};
+
 pub struct Latent {}
+
+#[async_trait]
+impl Network for Latent {
+    async fn send(
+        self: Arc<Self>,
+        delivery: Delivery,
+        attachments: &[Attachment],
+    ) -> Result<(), Box<dyn Error>> {
+        for attachment in attachments
+            .iter()
+            .filter(|attachment| attachment.machine != delivery.sender)
+        {
+            attachment.sender.send(delivery.clone()).await.unwrap();
+        }
+        Ok(())
+    }
+}
