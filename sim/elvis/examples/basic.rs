2148a558fefad3794c83697a6e593ec77ec1c325
Tim Harding <tim@timharding.co>
2022-12-07 16:32:56 -0800

    Use statement spacing for examples

--- sim/elvis/examples/basic.rs
+++ sim/elvis/examples/basic.rs
@@ -1,5 +1,6 @@
 use elvis::simulations::basic;
 use elvis_core::cli::parse_cli;
+
 #[tokio::main]
 async fn main() {
     parse_cli();
--- sim/elvis/examples/telephone_multi.rs
+++ sim/elvis/examples/telephone_multi.rs
@@ -1,5 +1,6 @@
 use elvis::simulations::telephone_multi;
 use elvis_core::cli::parse_cli;
+
 #[tokio::main]
 async fn main() {
     parse_cli();
