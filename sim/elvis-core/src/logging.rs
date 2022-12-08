d3f0741d20febddcb4ad576279289211f1b2ee08
Tim Harding <tim@timharding.co>
2022-12-07 17:09:51 -0800

    Logging code readability improvement

--- sim/elvis-core/src/logging.rs
+++ sim/elvis-core/src/logging.rs
@@ -18,7 +18,15 @@ pub fn send_message_event(
     remote_port: u16,
     message: Message,
 ) {
-    event!(target: "SEND_MESSAGE", Level::INFO, local_ip = format!("{:?}", local_ip.to_bytes()), remote_ip= format!("{:?}", remote_ip.to_bytes()), local_port= format!("{:x}", local_port), remote_port=format!("{:x}", remote_port), message=format!("{}", message));
+    event!(
+        target: "SEND_MESSAGE",
+        Level::INFO,
+        local_ip = format!("{:?}", local_ip.to_bytes()),
+        remote_ip= format!("{:?}", remote_ip.to_bytes()),
+        local_port= format!("{:x}", local_port),
+        remote_port=format!("{:x}", remote_port),
+        message=format!("{}", message),
+    );
 }
 
 /// Receive message event handler.
@@ -31,7 +39,15 @@ pub fn receive_message_event(
     remote_port: u16,
     message: Message,
 ) {
-    event!(target: "RECV_MESSAGE", Level::INFO, local_ip = format!("{:?}", local_ip.to_bytes()), remote_ip= format!("{:?}", remote_ip.to_bytes()), local_port= format!("{:x}", local_port), remote_port=format!("{:x}", remote_port), message=format!("{}", message));
+    event!(
+        target: "RECV_MESSAGE",
+        Level::INFO,
+        local_ip = format!("{:?}", local_ip.to_bytes()),
+        remote_ip= format!("{:?}", remote_ip.to_bytes()),
+        local_port= format!("{:x}", local_port),
+        remote_port=format!("{:x}", remote_port),
+        message=format!("{}", message)
+    );
 }
 
 // TODO: correlate the machine id's to IP's or protocols
@@ -40,5 +56,10 @@ pub fn receive_message_event(
 /// machine_id, list of all protocol id's associated with the machine
 /// This will eventually contain more info as the simulation evolves
 pub fn machine_creation_event(machine_id: usize, protocol_ids: Vec<ProtocolId>) {
-    event!(target: "MACHINE_CREATION", Level::INFO, machine_id=machine_id, protocol_ids = format!("{:?}", protocol_ids));
+    event!(
+        target: "MACHINE_CREATION",
+        Level::INFO,
+        machine_id=machine_id,
+        protocol_ids = format!("{:?}", protocol_ids),
+    );
 }
