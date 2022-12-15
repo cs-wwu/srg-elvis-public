f9c21d29a058f8cae6a6f1316d18b9863517d779
Tim Harding <tim@timharding.co>
2022-12-15 14:26:36 -0800

    Removed UDP-global error type

--- sim/elvis-core/src/protocols/udp/udp_misc.rs
+++ sim/elvis-core/src/protocols/udp/udp_misc.rs
@@ -3,7 +3,6 @@ use crate::control::{
     value::{from_impls, make_key},
     Key,
 };
-use thiserror::Error as ThisError;
 
 const LOCAL_PORT_KEY: Key = make_key("UDP Local Port");
 /// A [`control::Value`] for the local port number.
@@ -14,17 +13,3 @@ const REMOTE_PORT_KEY: Key = make_key("UDP Remote Port");
 /// A [`control::Value`] for the remote port number.
 pub type RemotePort = control::Value<REMOTE_PORT_KEY, u16>;
 from_impls!(RemotePort, u16);
-
-#[derive(Debug, ThisError)]
-pub(super) enum UdpError {
-    #[error("Too few bytes to constitute a UDP header")]
-    HeaderTooShort,
-    #[error(
-        "The computed checksum {actual:#06x} did not match the header checksum {expected:#06x}"
-    )]
-    InvalidChecksum { actual: u16, expected: u16 },
-    #[error("The number of message bytes differs from the header")]
-    LengthMismatch,
-    #[error("The UDP payload is longer than can fit into a single packet")]
-    OverlyLongPayload,
-}
