c0793ba66b0c869be154a34b7596eddd90c46249
Robin Preble <prebler@wwu.edu>
2023-01-11 13:54:10 -0800

    cleaned up comments a bit

--- tools/scraper/src/main.rs
+++ tools/scraper/src/main.rs
@@ -13,8 +13,8 @@ use clap::{Command, Arg};
 #[derive(Serialize, Deserialize, Debug)]
  struct Page {
     size: usize,
-    links: Vec<String>,  //list of all website urls found
-    images: Vec<String>, //list of all images urls found
+    links: Vec<String>,  // list of all website urls found
+    images: Vec<String>, // list of all images urls found
  }
  #[derive(Serialize, Deserialize, Debug)]
  struct Image{
@@ -26,7 +26,7 @@ use clap::{Command, Arg};
         Self { size, links, images}
     }
 
-    //get method for list of urls found on a page
+    // get method for list of urls found on a page
     fn get_urls(&mut self) -> & Vec<String>{
         &self.links
     }
@@ -49,26 +49,26 @@ use clap::{Command, Arg};
 fn filter_url(link: &str) -> Option<String>{
     let url = Url::parse(link);
     match  url {
-        //if the url is valid, aka has https:// then check if it points to yahoo.com
+        // if the url is valid, aka has https:// then check if it points to yahoo.com
         Ok(url) =>{
             if url.has_host() && url.host_str().unwrap().ends_with("yahoo.com") && !url.to_string().contains("beap.gemini"){       //points to yahoo
                 Some(url.to_string())
-            }else{ // discard if not yahoo-related
+            }else{  // discard if not yahoo-related
                 None
             }
         },
-        //if the url is not valid, add https:// to it so it can used with reqwest
+        // if the url is not valid, add https:// to it so it can used with reqwest
         Err(_e) =>{
             if link.starts_with("/"){//..or ends with .html
                 Some(format!("https://yahoo.com{}",link))
-            }else{//..not even a link, ex: javascript:void(0)
+            }else{  //..not even a link, ex: javascript:void(0)
                 None
             }
         }
     }
 }
 
-//discard any invalid image url
+// discard any invalid image url
 fn filter_img_url(link: &str) -> Option<String>{
     if link.contains("https://s.yimg.com") {
         Some(link.to_string())
@@ -77,8 +77,8 @@ fn filter_img_url(link: &str) -> Option<String>{
     }
 }
 
-//send http request to the url and receive response. Return html in string and the size of the page in bytes
-//if the response give error, tries the link again 3 time, if still fails, add to fail list
+// send http request to the url and receive response. Return html in string and the size of the page in bytes
+// if the response give error, tries the link again 3 time, if still fails, add to fail list
 fn http_requester(link: &str, mut tries:u32, baddies: &mut Vec<String>) -> Option<String>{
 
     if tries == 4{
@@ -89,20 +89,19 @@ fn http_requester(link: &str, mut tries:u32, baddies: &mut Vec<String>) -> Optio
     let client = reqwest::blocking::Client::new();
     let request = client.get(link)
     .header("User-Agent", "Mozilla/5.0")
-    .timeout(Duration::new(3, 0));  //if the request sent is hung for more than 3 seconds, stop and return time out error
+    .timeout(Duration::new(3, 0));  // if the request sent is hung for more than 3 seconds, stop and return time out error
 
     let response = request.send();
-    //println!("request sent!");
+    // println!("request sent!");
 
-    //had to manually handle error in case we get 404 url, which will make the program crash if we just use unwrap()
+    // had to manually handle error in case we get 404 url, which will make the program crash if we just use unwrap()
     match response {
         Ok(rep) =>{
             match rep.text(){
                 Ok(txt) =>{
-                    //println!("got text");
                     Some(txt)
                 },
-                Err(_e) =>{ //try the link 3 times then stop if still gives error
+                Err(_e) =>{ // try the link 3 times then stop if still gives error
                     println!("Fail! {}", _e);
                     tries +=1;
                     http_requester(link, tries, baddies)
@@ -118,14 +117,14 @@ fn http_requester(link: &str, mut tries:u32, baddies: &mut Vec<String>) -> Optio
 }
 
 
-//extract urls from the given html
-//change to Option<Vec<String>>? in case there's no link at all in a page???
+// extract urls from the given html
+// change to Option<Vec<String>>? in case there's no link at all in a page???
 fn extract_urls(html: &str) -> Vec<String>{
-    //form a html document
+    // form a html document
     let document = Document::from(html);
 
-    //extracting all links in the yahoo page and filter out bad urls
-    //NOTE: use HashMap to avoid duplicate value, aka visted pages
+    // extracting all links in the yahoo page and filter out bad urls
+    // NOTE: use HashMap to avoid duplicate value, aka visted pages
     let found_urls= document.find(Name("a"))
     .filter_map(|node| node.attr("href"))
     .filter_map(|link| filter_url(link))
@@ -134,7 +133,7 @@ fn extract_urls(html: &str) -> Vec<String>{
     return found_urls;
 }
 
-//extracting all images from a page
+// extracting all images from a page
 fn extract_images(html: &str) -> Vec<String>{
     let document = Document::from(html);
     
@@ -157,21 +156,17 @@ fn extract_images(html: &str) -> Vec<String>{
 fn download_img(img_urls: &Vec<String>, downloaded: &mut HashMap<String, Image>, baddies:&mut Vec<String>){
     for img in img_urls{
         if !downloaded.contains_key(img){
-
             println!("Processing IMG...{}", img);
 
-            //"download" the image
-            //let img_bytes = reqwest::blocking::get(img).unwrap().bytes().unwrap();
-
-            //TODO: check for error here instead of unwrap()
+            // "download" the image
             match reqwest::blocking::get(img) {
                 Ok(rep) => {
                     match rep.bytes() {
                         Ok(img_bytes) =>{
-                            //get size of image just downloaded and update the downloaded list
+                            // get size of image just downloaded and update the downloaded list
                             let size = img_bytes.len();
                             downloaded.insert(img.to_string(), Image::new(size));
-                            //testing
+                            // testing
                             println!("Success! -> size: {}",size);
                         },
                         Err(_e) =>{
@@ -238,27 +233,27 @@ fn bfs_scraper(link: &str, visited: &mut HashMap<String,Rc<Page>>, downloaded: &
     while !unvisited_urls.is_empty() && limit > 0 {
         let url = unvisited_urls.pop_front().unwrap();
 
-        println!("Processing URL...{}", url);      //checking which link is being scraped in case it crashes
+        println!("Processing URL...{}", url);  // checking which link is being scraped in case it crashes
 
         let res = http_requester(&url, 1, baddies);
-        if res.is_none(){//ignore invalid url 404
+        if res.is_none(){  // ignore invalid url 404
             continue;
         }
 
-        //scrape urls and imgs on the page
+        // scrape urls and imgs on the page
         let res_text = res.unwrap();
         let scraped_urls = extract_urls(&res_text);
         let scraped_imgs = extract_images(&res_text);
         let size = res_text.len();
 
-        //printing links in hashmap, should NOT have dups
+        // printing links in hashmap, should NOT have dups
         println!("Sucess! -> Size:{}", size);
 
-        //download all images found
+        // download all images found
         println!("*******Images found within this link*******");
         download_img(&scraped_imgs, downloaded, baddies);
 
-        //write page info to a log file
+        // write page info to a log file
         log_file.write_fmt(format_args!("URL: {} - Size: {}: ", &url, size)).expect("write url failed");
         log_file.write_fmt(format_args!("URLS List: {:?} ,", &scraped_urls)).expect("write url list failed");
         log_file.write_fmt(format_args!("IMG List: {:?} \n", &scraped_imgs)).expect("write images failed");
@@ -266,7 +261,7 @@ fn bfs_scraper(link: &str, visited: &mut HashMap<String,Rc<Page>>, downloaded: &
         
         let new_page = Rc::new(Page::new(size, scraped_urls, scraped_imgs));
         
-        //add unvisited urls from scraped_urls to found_urls
+        // add unvisited urls from scraped_urls to found_urls
         for this_url in &new_page.links{
             let stripped = String::from(strip_url(this_url));
 
@@ -290,27 +285,27 @@ fn bfs_scraper_with_limit(link: &str, visited: &mut HashMap<String,Rc<Page>>, do
     while !unvisited_urls.is_empty() && limit > 0 {
         let url = unvisited_urls.pop_front().unwrap();
 
-        println!("Processing URL...{}", url);      //checking which link is being scraped in case it crashes
+        println!("Processing URL...{}", url);  // checking which link is being scraped in case it crashes
 
         let res = http_requester(&url, 1, baddies);
-        if res.is_none(){//ignore invalid url 404
+        if res.is_none(){  // ignore invalid url 404
             continue;
         }
 
-        //scrape urls and imgs on the page
+        // scrape urls and imgs on the page
         let res_text = res.unwrap();
         let scraped_urls = extract_urls(&res_text);
         let scraped_imgs = extract_images(&res_text);
         let size = res_text.len();
 
-        //printing links in hashmap, should NOT have dups
+        // printing links in hashmap, should NOT have dups
         println!("Sucess! -> Size:{}", size);
 
-        //download all images found
+        // download all images found
         println!("*******Images found within this link*******");
         download_img(&scraped_imgs, downloaded, baddies);
 
-        //write page info to a log file
+        // write page info to a log file
         log_file.write_fmt(format_args!("URL: {} - Size: {}: ", &url, size)).expect("write url failed");
         log_file.write_fmt(format_args!("URLS List: {:?} ,", &scraped_urls)).expect("write url list failed");
         log_file.write_fmt(format_args!("IMG List: {:?} \n", &scraped_imgs)).expect("write images failed");
@@ -318,7 +313,7 @@ fn bfs_scraper_with_limit(link: &str, visited: &mut HashMap<String,Rc<Page>>, do
         
         let new_page = Rc::new(Page::new(size, scraped_urls, scraped_imgs));
         
-        //add unvisited urls from scraped_urls to found_urls
+        // add unvisited urls from scraped_urls to found_urls
         for this_url in &new_page.links{
             let stripped = String::from(strip_url(this_url));
 
@@ -332,11 +327,10 @@ fn bfs_scraper_with_limit(link: &str, visited: &mut HashMap<String,Rc<Page>>, do
     }
 }
 fn main() {
-    //parsing arguments using CLAP
+    // parsing arguments using CLAP
     let arg_matcher = Command::new("Web Crawl Test")
         .about("Web crawler")
         .author("Min Nguyen")
-        //.allow_missing_positional(true)
         .arg(Arg::with_name("max")
             .short('m')
             .long("max")
@@ -350,7 +344,7 @@ fn main() {
             .help("The url of the root website to crawl from"))
         .get_matches();
     
-    //fetching the url from the user: need to start with http:/ or https:/
+    // fetching the url from the user: need to start with http:/ or https:/
     let url = arg_matcher.value_of("url").unwrap();
     let http_head = &(url)[..4];
     
@@ -359,7 +353,7 @@ fn main() {
         return;
     }
     
-    //see how many page to be crawled
+    // see how many page to be crawled
     let max = arg_matcher.value_of("max");
     let mut limit = match max {
         None => {
@@ -384,28 +378,28 @@ fn main() {
         }
     };
 
-    //list of visited website
+    // list of visited website
     let mut visited: HashMap<String, Rc<Page>> = HashMap::new();
-    //list of downloaded images
+    // list of downloaded images
     let mut downloaded: HashMap<String, Image> = HashMap::new();
-    //list of failed URLs
+    // list of failed URLs
     let mut baddies: Vec<String> = Vec::new();
 
-    //file to write results to
+    // file to write results to
     let log_file = File::create("log.txt").unwrap();
     let pages_file = File::create("visited.json").unwrap();
     let imgs_file = File::create("downloaded.json").unwrap();
     let fails_file = File::create("baddies.json").unwrap();
     let found_urls_stripped = File::create("found_urls_stripped").unwrap(); 
     
-    //recursive_scraper(&url, &mut visited, &mut downloaded, &mut baddies);
+    // recursive_scraper(&url, &mut visited, &mut downloaded, &mut baddies);
     if limit == 0{
         bfs_scraper(&url, &mut visited, &mut downloaded, &mut baddies, log_file, found_urls_stripped);
     }else{
         bfs_scraper_with_limit(&url, &mut visited, &mut downloaded, &mut baddies, limit, log_file, found_urls_stripped);
     }
     
-    //serialize result as JSON string to the created paths
+    // serialize result as JSON string to the created paths
     let pages_cerealizer = serde_json::ser::to_writer_pretty(pages_file, &visited).unwrap();
     let imgs_cerealizer = serde_json::ser::to_writer_pretty(imgs_file, &downloaded).unwrap();
     let fail_cerealizer = serde_json::ser::to_writer_pretty(fails_file, &baddies).unwrap();
