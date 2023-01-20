601af1fbcac064d6134d1408de389e023ce25944
RobinPreble <robinpreble@outlook.com>
2023-01-20 15:16:24 -0800

    starting to figure out pandas, about to restructure code

--- tools/scraper/src/analyzer.py
+++ tools/scraper/src/analyzer.py
@@ -1,19 +1,44 @@
+"""
+Plan: 
+Make a series using panda with the following data for each link: 
+    - The actual link
+    - size of page
+    - num links 
+    - num images 
+    - size of each image on the page
+
+Then using that data use matplotlib to create pretty representations for
+    - size of pages
+    - num links
+    - num images
+    - average size of images 
+"""
+import pandas
+
 def parse_lines(lines):
+    dataset = {
+        'link': [],
+        'size': [], 
+        'numImages': [], 
+    }
+
     for line in lines: 
         if line.strip() == "{": 
             # do nothing
             print("start")
         elif line.startswith('  "'): 
             end = line.rindex('"')
-            print(line[3:end])
+            #print(line[3:end])
+            dataset["link"].append(line[3:end])
         elif line.startswith('    "size":'):
             end = line.rindex(',')
-            print("size: " + line[12:end])
+            #print("size: " + line[12:end])
+            dataset["size"].append(int(line[12:end]))
         elif line.startswith('    "size":'):
             end = line.rindex(',')
-            print("size: " + line[12:end])
+            #print("size: " + line[12:end])
 
-
+    print(dataset['size'])
 def main():
     f = open("test.txt", "r")
     lines = f.readlines()
