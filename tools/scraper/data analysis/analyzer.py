7803755d5511ad1de1e8c43a84702860139fa11e
RobinPreble <robinpreble@outlook.com>
2023-01-20 16:09:48 -0800

    created data frame and moved around files a bit

--- tools/scraper/data analysis/analyzer.py
+++ tools/scraper/data analysis/analyzer.py
@@ -64,39 +64,18 @@ def parse_lines(lines):
             link_data.append(line)
     return dataset
 
-def old_parse_lines(lines):
-    dataset = {
-        'link': [],
-        'size': [], 
-        'numLinks': [],
-        'numImages': [],
-    }
-
-    for line in lines: 
-        if line.strip() == "{": 
-            # do nothing
-            print("start")
-        elif line.startswith('  "'): 
-            end = line.rindex('"')
-            #print(line[3:end])
-            dataset["link"].append(line[3:end])
-        elif line.startswith('    "size":'):
-            end = line.rindex(',')
-            #print("size: " + line[12:end])
-            dataset["size"].append(int(line[12:end]))
-        elif line.startswith('    "size":'):
-            end = line.rindex(',')
-            #print("size: " + line[12:end])
-
-    print(dataset['size'])
 def main():
     f = open("test.txt", "r")
     lines = f.readlines()
     dataset = parse_lines(lines)
+    """
     print(dataset['link'])
     print(dataset['size'])
     print(dataset['numLinks'])
     print(dataset['numImages'])
+    """
+    dataframe = pandas.DataFrame(dataset)
+    print(dataframe)
 
     f.close()
 
