e5077f736fbba8d5137753c488a2619c287a5e16
RobinPreble <robinpreble@outlook.com>
2023-01-20 16:03:16 -0800

    successfully parsing data and putting it into a dictionary

--- tools/scraper/src/analyzer.py
+++ tools/scraper/src/analyzer.py
@@ -16,30 +16,59 @@ Then using that data use matplotlib to create pretty representations for
 import pandas
 
 def parse_link_data(link_data, dataset): 
-    print("------------------------")
-    print(link_data)
+    link_count = 0
+    image_count = 0
+    counting_links = False
+    counting_images = False
+    for line in link_data: 
+        if counting_links: 
+            if line.strip() == '],':
+                dataset['numLinks'].append(link_count)
+                counting_links = False
+            else:
+                link_count += 1
+        elif counting_images:
+            if line.strip() == ']':
+                dataset['numImages'].append(image_count)
+                counting_images = False
+            else:
+                image_count += 1
+        else: 
+            if line.startswith('  "'): 
+                end = line.rindex('"')
+                dataset["link"].append(line[3:end])
+            elif line.startswith('    "size":'): 
+                end = line.rindex(',')
+                dataset["size"].append(int(line[12:end]))
+            elif line.strip() == '"links": [':
+                counting_links = True
+            elif line.strip() == '"images": [':
+                counting_images = True
 
 
 def parse_lines(lines):
     dataset = {
         'link': [],
         'size': [], 
-        'numImages': [],
+        'numLinks': [],
+        'numImages': []
     }
-    link_data = ''
+    link_data = []
     for line in lines: 
         if line.strip() == '{' or line.strip() == '}': 
             continue
         elif line.strip() == '},': 
             parse_link_data(link_data, dataset)
-            link_data = ''
+            link_data = []
         else:
-            link_data = link_data + line
+            link_data.append(line)
+    return dataset
 
 def old_parse_lines(lines):
     dataset = {
         'link': [],
         'size': [], 
+        'numLinks': [],
         'numImages': [],
     }
 
@@ -63,7 +92,12 @@ def old_parse_lines(lines):
 def main():
     f = open("test.txt", "r")
     lines = f.readlines()
-    parse_lines(lines)
+    dataset = parse_lines(lines)
+    print(dataset['link'])
+    print(dataset['size'])
+    print(dataset['numLinks'])
+    print(dataset['numImages'])
+
     f.close()
 
 
