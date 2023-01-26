437cbceabab01e78decfbba082ae58f88365ac37
Zach <zachd9757@gmail.com>
2023-01-26 13:04:37 -0800

    Setup for implementing Protocol trait

--- sim/elvis-core/src/protocols/dns.rs
+++ sim/elvis-core/src/protocols/dns.rs
@@ -6,12 +6,46 @@ pub struct Dns {
 
 impl Dns {
     /// A unique identifier for the protocol.
-    pub const ID: Id = Id::new(4);
+    pub const ID: Id = Id::from_string("DNS");
 
     /// Creates a new instance of the protocol.
     pub fn new() -> Self {
         Self {
-
+            
         }
     }
+}
+
+impl Protocol for Dns {
+    fn id(self: Arc<Self>) -> Id {
+        Self::ID
+    }
+
+    fn open(
+        self: Arc<Self>,
+        upstream: Id,
+        participants: Control,
+        protocols: ProtocolMap,
+    ) -> Result<SharedSession, OpenError> {
+        //TODO
+    }
+
+    fn listen(
+        self: Arc<Self>,
+        upstream: Id,
+        participants: Control,
+        protocols: ProtocolMap,
+    ) -> Result<(), ListenError> {
+        //TODO
+    }
+
+    fn demux(
+        self: Arc<Self>,
+        message: Message,
+        caller: SharedSession,
+        context: Context,
+    ) -> Result<(), DemuxError> {
+        //TODO
+    }
+
 }
