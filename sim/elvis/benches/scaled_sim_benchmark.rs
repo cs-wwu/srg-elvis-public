888fe09d5241bcc4acddcdfc539991acc4058e7d
jacob-hollands19 <73252388+jacob-hollands19@users.noreply.github.com>
2023-01-28 14:40:13 -0800

    fmt update

--- sim/elvis/benches/scaled_sim_benchmark.rs
+++ sim/elvis/benches/scaled_sim_benchmark.rs
@@ -3,12 +3,9 @@
 
 // use tokio::runtime::Runtime;
 
-
 // fn ten_machines(c: &mut Criterion) {
-//     let file_path = "./benches/ten_machines.txt"; 
+//     let file_path = "./benches/ten_machines.txt";
 //     c.bench_with_input(BenchmarkId::new("generate_sim", file_path), &file_path, |b, &s| {
-//         // Insert a call to `to_async` to convert the bencher to async mode.
-//         // The timing loops are the same as with the normal bencher.
 //         b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
 //     });
 // }
