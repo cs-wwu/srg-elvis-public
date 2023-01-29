46baed7e763dd9f29d4a2eba81ad1c5c24df1860
jacob-hollands19 <73252388+jacob-hollands19@users.noreply.github.com>
2023-01-29 14:15:51 -0800

    Added time benchmarking for some large scale sims
    Sims being testes are 10, 100, 1000, 10000, 50000, 100000 machines respectively
    
    Co-Authored-By: mitchell-thompson-CS <55821824+mitchell-thompson-CS@users.noreply.github.com>

--- sim/elvis/benches/scaled_sim_benchmark.rs
+++ sim/elvis/benches/scaled_sim_benchmark.rs
@@ -1,17 +1,70 @@
-// use elvis::ndl::{generate_sim};
-// use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{
+    criterion_group, criterion_main, BenchmarkId, Criterion, PlotConfiguration, SamplingMode,
+};
+use elvis::ndl::generate_sim;
 
-// use tokio::runtime::Runtime;
+use tokio::runtime::Runtime;
 
-// fn ten_machines(c: &mut Criterion) {
-//     let file_path = "./benches/ten_machines.txt";
-//     c.bench_with_input(BenchmarkId::new("generate_sim", file_path), &file_path, |b, &s| {
-//         b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
-//     });
-// }
+fn benchmark_sim(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Large Scale Sims");
+    let file_path_ten = "./benches/ndl_benchmarks/ten_machines.txt";
+    let file_path_hundred = "./benches/ndl_benchmarks/hundred_machines.txt";
+    let file_path_thousand = "./benches/ndl_benchmarks/thousand_machines.txt";
+    let file_path_ten_thousand = "./benches/ndl_benchmarks/ten_thousand_machines.txt";
+    let file_path_fifty_thousand = "./benches/ndl_benchmarks/fifty_thousand_machines.txt";
+    let file_path_hundred_thousand = "./benches/ndl_benchmarks/hundred_thousand_machines.txt";
+    // group.sampling_mode(SamplingMode::Flat);
+    // group.sample_size(50);
+    group
+        .plot_config(PlotConfiguration::default().summary_scale(criterion::AxisScale::Logarithmic));
+    group.bench_with_input(
+        BenchmarkId::new("generate_sim", file_path_ten),
+        &file_path_ten,
+        |b, &s| {
+            b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("generate_sim", file_path_hundred),
+        &file_path_hundred,
+        |b, &s| {
+            b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("generate_sim", file_path_thousand),
+        &file_path_thousand,
+        |b, &s| {
+            b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("generate_sim", file_path_ten_thousand),
+        &file_path_ten_thousand,
+        |b, &s| {
+            b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("generate_sim", file_path_fifty_thousand),
+        &file_path_fifty_thousand,
+        |b, &s| {
+            b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("generate_sim", file_path_hundred_thousand),
+        &file_path_hundred_thousand,
+        |b, &s| {
+            b.to_async(runtime()).iter(|| generate_sim(s.to_string()));
+        },
+    );
+    group.finish();
+}
 
-// fn runtime() -> Runtime {
-//     Runtime::new().unwrap()
-// }
-// criterion_group!(benches, ten_machines);
-// criterion_main!(benches);
+fn runtime() -> Runtime {
+    Runtime::new().unwrap()
+}
+
+criterion_group!(benches, benchmark_sim);
+criterion_main!(benches);
