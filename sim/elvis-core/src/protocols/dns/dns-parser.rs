a214902bf775dd1f3a5fbaf2de760c1d5d89d65b
Zach <zachd9757@gmail.com>
2023-02-02 12:28:40 -0800

    impl DnsHeader: from_bytes

--- sim/elvis-core/src/protocols/dns/dns-parser.rs
+++ sim/elvis-core/src/protocols/dns/dns-parser.rs
@@ -2,6 +2,12 @@ use super::Dns;
 use crate::bit;
 
 
+
+/// The number of `u32` words in a basic DNS header
+const BASE_WORDS: u8 = 6;
+/// The number of `u8` bytes in a basic IPv4 header
+const BASE_OCTETS: u16 = BASE_WORDS as u16 * 2;
+
 /// A DNS header, as described in RFC1035 p25 s4.1.1
 pub(super) struct DnsHeader {
     /// A 16 bit identifier assigned by the program that
@@ -20,15 +26,30 @@ pub(super) struct DnsHeader {
     pub nscount: u16,
     /// the number of resource records in the additional records section.
     pub arcount: u16,
-    
+}
 
+impl DnsHeader {
+    /// Parses a header from a byte iterator.
+    pub fn from_bytes(mut bytes: impl Iterator<Item = u8>) -> Result<Self, ParseError> {
+        let mut next =
+            || -> Result<u8, ParseError> { bytes.next().ok_or(ParseError::HeaderTooShort) };
+
+
+        let id = u16::from_be_bytes([next()?, next()?]);
+    }
+}
+
+#[derive(Debug, ThisError, Clone, Copy, PartialEq, Eq)]
+pub enum ParseError {
+    #[error("The DNS header is incomplete")]
+    HeaderTooShort,
 }
 
 /// Wrapper struct for holding the QR, Opcode, AA, TC, RD, RA, Z, and RCODE
 /// fields of a DNS Header.
-pub(super) struct DNSHeaderProperties(u16);
+pub(super) struct DnsHeaderProperties(u16);
 
-impl DNSHeaderProperties {
+impl DnsHeaderProperties {
     fn get_QR() -> bool {
         self.bit(0)
     }
@@ -60,8 +81,8 @@ impl DNSHeaderProperties {
     }
 }
 
-impl BitIndex for DNSHeaderProperties {
-    /// DNSHeaderProperties are defined by 2 bytes, 16 bits
+impl BitIndex for DnsHeaderProperties {
+    /// DnsHeaderProperties are defined by 2 bytes, 16 bits
     fn bit_length() -> usize {
         2
     }
@@ -83,7 +104,6 @@ impl BitIndex for DNSHeaderProperties {
     }
 
 }
-
 
 #[cfg(test)]
 mod tests {
