07e79dbba4f4712bfb6997a08fa397c83f54c409
giddinl2 <giddinl2@wwu.edu>
2023-03-01 14:22:40 -0800

    Initial Sockets API
    Only "conectionless" sockets are implemented so far, listen() and accept() are not implemented
    Sockets can still be used to communicate data via explicit bind()ing and connect()ing to specific IP addresses/ports
    Functions:
    - Sockets::new_socket - Creates a new Socket
    - Socket::bind - Binds a socket to a local IP and port
    - Socket::connect - Connects a bound socket to a remote IP and port
    - Socket::send - Sends data via a connected socket
    - Socket::recv - Receives data via a connected socket
    - Socket::recv_msg - Receives a Message struct via a connected socket
    - Socket::listen - Not yet implemented, will panic if called
    - Socket::accept - Not yet implemented, will panic if called

---
+++ sim/elvis-core/src/protocols/sockets/socket_session.rs
@@ -0,0 +1,46 @@
+use std::sync::Arc;
+
+use crate::{
+    session::{ SharedSession, SendError, QueryError },
+    Message,
+    protocol::{ Context, DemuxError },
+    Session,
+    control::{ Key, Primitive }
+};
+
+use super::socket::Socket;
+
+pub(super) struct SocketSession {
+    pub upstream: Arc<Socket>,
+    pub downstream: SharedSession
+}
+
+impl SocketSession {
+    pub fn receive(self: Arc<Self>, message: Message, _context: Context) -> Result<(), DemuxError> {
+        /* receive_message_event(
+            self.id.local.address,
+            self.id.remote.address,
+            self.id.local.port,
+            self.id.remote.port,
+            message.clone(),
+        );
+        context
+            .protocol(self.upstream)
+            .expect("No such protocol")
+            .demux(message, self, context)?; */
+        match self.upstream.receive(message) {
+            Ok(v) => Ok(v),
+            Err(_e) => Err(DemuxError::Other) 
+        }
+    }
+}
+
+impl Session for SocketSession {
+    fn send(self: Arc<Self>, message: Message, context: Context) -> Result<(), SendError> {
+        self.downstream.clone().send(message, context)
+    }
+
+    fn query(self: Arc<Self>, key: Key) -> Result<Primitive, QueryError> {
+        self.downstream.clone().query(key)
+    }
+}
---
+++ sim/elvis/src/simulations/socket_basic.rs
@@ -0,0 +1,54 @@
+use crate::applications::{SocketSendMessage, SocketRecvMessage};
+use elvis_core::{
+    protocol::SharedProtocol,
+    protocols::{
+        ipv4::{IpToTapSlot, Ipv4, Ipv4Address},
+        udp::Udp,
+        Pci,
+        Sockets,
+    },
+    run_internet, Machine, Network
+};
+
+/// Runs a two-way communication simulation using sockets.
+///
+/// In this simulation, a machine sends a "request" message to another machine.
+/// The second machine receives the message, and sends back a "response" message.
+/// The first machine receives that message, and sends back a "shutdown" message.
+/// Finally, the second machine receives the "shutdown" message, and shuts down the simulation.
+pub async fn socket_basic() {
+    let network = Network::basic();
+    let send_ip_address: Ipv4Address = [123, 45, 67, 90].into();
+    let recv_ip_address: Ipv4Address = [123, 45, 67, 89].into();
+    let ip_table: IpToTapSlot = [(recv_ip_address, 0), (send_ip_address, 0)].into_iter().collect();
+
+    // let capture = Capture::new_shared(capture_ip_address, 0xbeef);
+    let send_socket_api = Sockets::new_shared(Some(send_ip_address));
+    let recv_socket_api = Sockets::new_shared(Some(recv_ip_address));
+    let machines = vec![
+        Machine::new([
+            send_socket_api.clone(),
+            Udp::new_shared() as SharedProtocol,
+            Ipv4::new_shared(ip_table.clone()),
+            Pci::new_shared([network.tap()]),
+            SocketSendMessage::new_shared(send_socket_api.clone(), "Ground Control to Major Tom", send_ip_address, 0xface, recv_ip_address, 0xbeef)
+        ]),
+        Machine::new([
+            recv_socket_api.clone(),
+            Udp::new_shared() as SharedProtocol,
+            Ipv4::new_shared(ip_table),
+            Pci::new_shared([network.tap()]),
+            SocketRecvMessage::new_shared(recv_socket_api.clone(), "Major Tom to Ground Control", recv_ip_address, 0xbeef, send_ip_address, 0xface)
+        ]),
+    ];
+
+    run_internet(machines, vec![network]).await;
+}
+
+#[cfg(test)]
+mod tests {
+    #[tokio::test]
+    async fn socket_basic() {
+        super::socket_basic().await
+    }
+}
