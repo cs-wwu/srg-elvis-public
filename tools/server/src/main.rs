65772ac4bbe07a2d64ad32c732c4a65aeb5eb6a9
RobinPreble <robinpreble@outlook.com>
2023-04-25 15:20:45 -0700

    Added more comments

--- tools/server/src/main.rs
+++ tools/server/src/main.rs
@@ -1,6 +1,5 @@
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
-use std::sync::mpsc::RecvError;
 use std::{
     fs,
     fs::File,
@@ -11,30 +10,33 @@ use std::{
 };
 use rand_distr::WeightedAliasIndex;
 use rand::prelude::*;
-use csv::{Reader, StringRecord};
+use csv::Reader;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    // Process each connection 
+    // Process each stream
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
     }
 }
 
-// Send the html page data over the stream
+/* Read the incoming requests and send back the appropriate response */
 fn handle_connection(mut stream: TcpStream) {
+    // Recieve request
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let status_line = "HTTP/1.1 200 OK";
 
+    // Parse request and prepare response
     let contents = if request_line.contains(".jpg") { // image request, send bytes back
-        get_bytes(generate_number("image_size_weights.csv").unwrap())
-    } else {  // html page request 
+        get_bytes(generate_number("image_size.csv").unwrap())
+    } else {  // html page request, send html page back
         generate_html();
         fs::read_to_string("page.html").unwrap()
     };
 
+    // Send response 
     let length = contents.len();
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
@@ -50,10 +52,12 @@ fn generate_html() {
     let empty_page_bytes = 151;
     let image_bytes = 22;
 
-    let size = generate_number("size_weights.csv").unwrap();
-    let num_links = generate_number("num_links_weights.csv").unwrap();
-    let num_images = generate_number("num_images_weights.csv").unwrap();
+    // Generate html page characteristics
+    let size = generate_number("page_size.csv").unwrap();
+    let num_links = generate_number("num_links.csv").unwrap();
+    let num_images = generate_number("num_images.csv").unwrap();
 
+    // Assemble the html file contents
     result += "<!DOCTYPE html>
     <html lang=\"en\">
       <head>
@@ -61,26 +65,26 @@ fn generate_html() {
         <title>page</title>
       </head>
       <body>\n";
-    for link in generate_links(num_links) {
+    for link in generate_links(num_links) { // insert links
         result += "<a href=\"";
         result += &link;
         result += "\">a</a>\n";
     }
-    for img in generate_links(num_images) {
+    for img in generate_links(num_images) { // insert images
         result += "<img src=\"http://127.0.0.1:7878";
         result += &img;
         result += ".jpg\">";
     }
-    
+    // Add the appropriate number of bytes of data if the current page size is < size
     let current_page_size = empty_page_bytes + (link_bytes * num_links) + (image_bytes * num_images);
-
     if current_page_size < size {
         result.push_str(get_bytes(size - current_page_size).as_str());
     }
     result += "<body> \n</html>";
     
+    // Write the result to a file 
     let mut file = File::create("page.html").unwrap();
-    file.write_all(result.as_bytes());
+    file.write_all(result.as_bytes()).unwrap();
 }
 
 // Generates a vector of links
@@ -100,6 +104,7 @@ fn generate_links(num_links: usize) -> Vec<String> {
     links
 }
 
+/* Generates a string containing num_bytes 0's */
 fn get_bytes(num_bytes: usize) -> String {
     let mut result = String::new();
     for _byte in 0..num_bytes {
@@ -108,17 +113,24 @@ fn get_bytes(num_bytes: usize) -> String {
     result
 }
 
-// Radomly generate a number based on the distribution in the given .csv file
+/* Returns a randomly selected number based on the distribution specified in data_file. 
+    data_file is a .csv file where each row is of the format bucket,weight where
+    bucket is a numerical value and weight is the number of data points that have that
+    value. The number returned will be one of the bucket values. Buckets with higher
+    weights are more likely to be selected. */
 fn generate_number(data_file: &str) -> Result<usize, Box<dyn Error>> {
     let mut buckets: Vec<f64> = Vec::new();
     let mut weights: Vec<usize> = Vec::new();
 
+    // Read from the file
     let mut rdr = Reader::from_path(data_file)?;
     for result in rdr.records() {
         let record = result?;
         buckets.push(record.get(0).unwrap().parse::<f64>().unwrap());
         weights.push(record.get(1).unwrap().parse::<usize>().unwrap());
     }
+
+    // Randomly select the value to be returned
     let dist = WeightedAliasIndex::new(weights).unwrap();
     let mut rng = thread_rng();
     
