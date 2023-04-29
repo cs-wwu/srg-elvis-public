8ded4a616c9a5105c234535175ee5ec41e01e05e
Tim Harding <tim@timharding.co>
2023-04-29 11:22:58 -0700

    Wrote fragmentation procedure

--- sim/elvis-core/src/protocols/ipv4/fragmentation.rs
+++ sim/elvis-core/src/protocols/ipv4/fragmentation.rs
@@ -4,49 +4,91 @@
 use super::ipv4_parsing::Ipv4Header;
 use crate::{network::Mtu, Message};
 
+type Fragment = (Ipv4Header, Message);
+
 /// The result of packet fragmentation
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Fragments {
     /// The packet was fragmented
-    Fragmented(Vec<(Ipv4Header, Message)>),
+    Fragmented(Vec<Fragment>),
     /// The packet does not require fragmentation
-    DontFragment(Ipv4Header, Message),
+    DontFragment(Fragment),
     /// The packet required fragmentation but requested not to be fragmented
     Discard,
 }
 
-pub fn fragment(mut header: Ipv4Header, mut body: Message, mtu: Mtu) -> Fragments {
+struct Fragmentation {
+    fragments: Vec<Fragment>,
+    mtu: Mtu,
+}
+
+impl Fragmentation {
+    fn new(mtu: Mtu) -> Self {
+        Self {
+            fragments: vec![],
+            mtu,
+        }
+    }
+
+    fn fragment(&self, mut header: Ipv4Header, mut body: Message) {
+        if header.total_length <= self.mtu {
+            self.fragments.push((header, body));
+            return;
+        }
+
+        // (2) Copy old values
+        let mut oihl = header.ihl;
+        let mut otl = header.total_length;
+        let mut ofo = header.fragment_offset;
+        let mut omf = header.flags.may_fragment();
+
+        // (3) Number of fragment blocks
+        let mut nfb = (self.mtu - header.ihl as u16 * 4) / 8;
+
+        // First fragment
+        {
+            // (1)
+            let mut header = header.clone();
+
+            // (4)
+            let body = body.cut(nfb as usize * 8);
+
+            // (5)
+            header.flags.set_may_fragment(true);
+            header.total_length = header.ihl as u16 * 4 + nfb * 8;
+
+            // (6)
+            self.fragments.push((header, body))
+        }
+
+        // (7)
+        // TODO(hardint): Remove options that should not be copied
+
+        // (8)
+        // Use whatever is left in `body`
+
+        // (9) Correct the header
+        //
+        // TODO(hardint): Recompute IHL after removing the not copied options:
+        // IHL <- (((OIHL*4)-(length of options not copied))+3)/4
+        //
+        // Note: Checksum recomputation happens at serialization time
+        header.total_length = otl - nfb * 8 - (oihl - header.ihl) as u16 * 4;
+        header.fragment_offset = ofo + nfb;
+
+        // (10)
+        self.fragment(header, body);
+    }
+}
+
+pub fn fragment(header: Ipv4Header, body: Message, mtu: Mtu) -> Fragments {
     if header.total_length <= mtu {
-        return Fragments::DontFragment(header, body);
+        Fragments::DontFragment((header, body))
+    } else if !header.flags.may_fragment() {
+        Fragments::Discard
+    } else {
+        let fragmentation = Fragmentation::new(mtu);
+        fragmentation.fragment(header, body);
+        Fragments::Fragmented(fragmentation.fragments)
     }
-
-    if !header.flags.may_fragment() {
-        return Fragments::Discard;
-    }
-
-    // (2) Copy old values
-    let mut oihl = header.ihl;
-    let mut otl = header.total_length;
-    let mut ofo = header.fragment_offset;
-    let mut omf = header.flags.may_fragment();
-
-    // (3) Number of fragment blocks
-    let mut nfb = (mtu - header.ihl as u16 * 4) / 8;
-
-    let mut fragments = {
-        // (1)
-        let mut first_header = header.clone();
-
-        // (4)
-        let first_body = body.cut(nfb as usize * 8);
-
-        // (5) Correct the header
-        first_header.flags.set_may_fragment(true);
-        first_header.total_length = first_header.ihl as u16 * 4 + nfb as u16 * 8;
-        vec![(first_header, first_body)]
-    };
-
-    while header.total_length > mtu {}
-
-    Fragments::Fragmented(fragments)
 }
