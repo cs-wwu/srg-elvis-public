ec3e3710a4c04234f7047d9dcb5bf4ed63c3ba8f
Tim Harding <tim@timharding.co>
2023-04-30 15:43:50 -0700

    Fixed steps 10 and 14 of segment reassembly

--- sim/elvis-core/src/protocols/ipv4/reassembly/segment.rs
+++ sim/elvis-core/src/protocols/ipv4/reassembly/segment.rs
@@ -1,3 +1,5 @@
+//! References page 28 of RFC 791: https://www.rfc-editor.org/rfc/rfc791
+
 use super::{bitvec::BitVec, fragment::Fragment};
 use crate::{protocols::ipv4::ipv4_parsing::Ipv4Header, Message};
 use std::{collections::BinaryHeap, time::Duration};
@@ -59,34 +61,50 @@ impl Segment {
         header: Ipv4Header,
         body: Message,
     ) -> Option<(Ipv4Header, Message)> {
-        // (8)
+        // (8) put data from fragment into data buffer with BUFID from octet
+        // FO*8 to octet (TL-(IHL*4))+FO*8;
+        //
+        // TODO(hardint): Does not correctly handle fragment blocks that arrive
+        // more than once
         self.fragments
             .push(Fragment::new(body, header.fragment_offset));
 
-        // (9)
+        // (9) set RCVBT bits from FO to FO+((TL-(IHL*4)+7)/8)
         self.fragment_blocks.set_range(
             header.fragment_offset,
             header.fragment_offset + (header.total_length - header.ihl as u16 * 4 + 7) / 8,
         );
 
-        // (10)
+        // (10) IF MF = 0 THEN TDL <- TL-(IHL*4)+(FO*8)
         if header.flags.is_last_fragment() {
-            self.total_data_length = header.total_length + header.fragment_offset * 8;
+            self.total_data_length =
+                header.total_length - header.ihl as u16 * 4 + header.fragment_offset * 8;
         }
 
-        // (11)
+        // (11) IF FO = 0 THEN put header in header buffer
         if header.fragment_offset == 0 {
             self.header = Some(header);
         }
 
-        // (12), (13)
-        if self.total_data_length != 0 && self.fragment_blocks.complete(self.total_data_length) {
-            // (14)
+        println!(
+            "{}, {}",
+            self.fragment_blocks.count(),
+            (self.total_data_length + 7) / 8
+        );
+
+        // (12) IF TDL # 0
+        // (13) AND all RCVBT bits from 0 to (TDL+7)/8 are set
+        if self.total_data_length != 0
+            && self
+                .fragment_blocks
+                .complete((self.total_data_length + 7) / 8)
+        {
+            // (14) THEN TL <- TDL+(IHL*4)
             let mut header = self.header.unwrap();
-            header.total_length = self.total_data_length;
+            header.total_length = self.total_data_length + header.ihl as u16 * 4;
             header.flags.set_is_last_fragment(true);
 
-            // (15)
+            // (15) Submit datagram to next step
             let mut message = Message::new(vec![]);
             while let Some(piece) = self.fragments.pop() {
                 message.concatenate(piece.into_message());
@@ -94,10 +112,9 @@ impl Segment {
 
             Some((header, message))
         } else {
-            // (17)
+            // (17) TIMER <- MAX(TIMER,TTL)
             self.epoch += 1;
             self.timeout_seconds = self.timeout_seconds.max(header.time_to_live);
-
             None
         }
     }
@@ -151,7 +168,7 @@ mod tests {
         let bytes_a: Vec<_> = (0..LEN).map(|i| i as u8).collect();
         let expected_a = Message::new(bytes_a);
 
-        let header_a = TestHeaderBuilder::new(LEN).build();
+        let header_a = TestHeaderBuilder::with_message_len(LEN).build();
         let a = match fragment(header_a, expected_a.clone(), MTU) {
             Fragments::Fragmented(fragments) => fragments,
             _ => panic!("Expected fragments"),
