419a066747fdc221b9934d938c816f7d6d4c3e53
Tim Harding <tim@timharding.co>
2023-04-29 17:03:21 -0700

    Commenting the reassembly code

--- sim/elvis-core/src/protocols/ipv4/reassembly.rs
+++ sim/elvis-core/src/protocols/ipv4/reassembly.rs
@@ -13,25 +13,38 @@ use std::{
     time::Duration,
 };
 
+/// Marks a particular call to [`Reassembly::add_fragment`]. Used to prevent
+/// reassembly resources from being cleared if new fragments came in before a
+/// timeout expired.
 pub type Epoch = u16;
 
 /// Timer lower bound
 const TLB: Duration = Duration::from_secs(15);
 
+/// Manages the reassembly of fragmented IP packets.
 #[derive(Debug, Default, Clone)]
 pub struct Reassembly {
-    pending: FxHashMap<BufId, Pending>,
+    /// Fragmented IP packets that are still waiting on fragments to become
+    /// complete.
+    pending: FxHashMap<BufId, Segment>,
 }
 
+/// Uniquely identifies the fragments of a particular datagram. See the
+/// Identification section for more details.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct BufId {
+    /// The remote IP address
     src: Ipv4Address,
+    /// The local IP address
     dst: Ipv4Address,
+    /// The transmission protocol used upstream from IP
     protocol: u8,
+    /// The identification field of the IP header
     identification: u16,
 }
 
 impl BufId {
+    /// Gets the segment identifier to a given IP header
     pub fn from_header(header: &Ipv4Header) -> Self {
         Self {
             src: header.source,
@@ -59,27 +72,21 @@ impl Reassembly {
         let pending = self
             .pending
             .entry(buf_id)
-            .or_insert_with(|| Pending::new(fragments));
+            .or_insert_with(|| Segment::new(fragments));
 
         // (8)
         pending
-            .pieces
-            .push(Piece::new(body, header.fragment_offset));
+            .fragments
+            .push(Fragment::new(body, header.fragment_offset));
 
         // (9)
-        pending.fragments_received.set_range(
+        pending.fragment_blocks.set_range(
             header.fragment_offset,
             header.fragment_offset + ((header.total_length - header.ihl as u16 * 4) + 7) / 8,
         );
 
-        // (10)
-        //
-        // TODO(hardint): Is this needed? I feel like we can figure this out
-        // when the first segment arrives. Also not sure if TDL is really needed
-        // at all. It is probably implicit from the assembled message length.
-        if header.flags.is_last_fragment() {
-            pending.tdl = header.total_length - header.ihl as u16 * 4 + header.fragment_offset * 8;
-        }
+        // (10) Ignored. We just find this value when initially constructing the
+        // Fragment.
 
         // (11)
         if header.fragment_offset == 0 {
@@ -87,17 +94,17 @@ impl Reassembly {
         }
 
         // (12), (13)
-        if pending.tdl != 0 && pending.fragments_received.complete() {
+        if pending.total_data_length() != 0 && pending.fragment_blocks.complete() {
             // (14)
             let mut header = pending.header.unwrap();
-            header.total_length = pending.tdl + header.ihl as u16 * 4;
+            header.total_length = pending.total_data_length() + header.ihl as u16 * 4;
 
             // (16)
             let pending = self.pending.remove(&buf_id).unwrap();
 
             // (15)
             let mut message = Message::new(vec![]);
-            for piece in pending.pieces.into_iter() {
+            for piece in pending.fragments.into_iter() {
                 message.concatenate(piece.message);
             }
             return AddFragmentResult::Complete(header, message);
@@ -114,7 +121,9 @@ impl Reassembly {
         AddFragmentResult::Incomplete(pending.timer, buf_id, epoch)
     }
 
-    pub fn maybe_cull_pending(&mut self, buf_id: BufId, epoch: Epoch) {
+    /// Removes the resources associated with the given [`BufId`] if no new
+    /// fragments have arrived since the given epoch.
+    pub fn maybe_cull_segment(&mut self, buf_id: BufId, epoch: Epoch) {
         match self.pending.entry(buf_id) {
             Entry::Occupied(pending) => {
                 if pending.get().epoch == epoch {
@@ -136,76 +145,91 @@ pub enum AddFragmentResult {
     Incomplete(Duration, BufId, Epoch),
 }
 
+/// Reassembly resources for a given [`BufId`] datagram identifier.
 #[derive(Debug, Clone)]
-struct Pending {
+struct Segment {
     header: Option<Ipv4Header>,
-    /// Tracks which fragments have been received
-    fragments_received: BitSet,
+    /// Tracks which fragments have been received. Each bit represent eight
+    /// consecutive bytes of the datagram, corresponding to values of the
+    /// fragment offset field in an IP header.
+    fragment_blocks: BitVec,
     /// Pieces which, taken collectively, will constitute the reassembled
     /// message. May be received out-of-order, hence we do not store an
     /// assembled message directly. The Piece type is set up such that once we
     /// have received all the fragments for a given message, we can just pop all
     /// the messages out of the heap and put them together end-to-end.
-    pieces: BinaryHeap<Piece>,
+    fragments: BinaryHeap<Fragment>,
     /// How long the pending message should be stored before being freed
     timer: Duration,
-    /// Total data length
-    tdl: u16,
+    /// The current iteration of this data structure. Incremented each time a
+    /// fragment arrives.
     epoch: u16,
 }
 
-impl Pending {
+impl Segment {
+    /// Creates a new set of reassembly resources for the given segment length
     pub fn new(len: u16) -> Self {
         Self {
             header: None,
-            fragments_received: BitSet::new(len),
-            pieces: Default::default(),
+            fragment_blocks: BitVec::new(len),
+            fragments: Default::default(),
             timer: TLB,
-            tdl: 0,
             epoch: 0,
         }
     }
+
+    /// The length of the final segment to be assembled.
+    pub fn total_data_length(&self) -> u16 {
+        self.fragment_blocks.len
+    }
 }
 
+/// A piece of a datagram being assembled. This type is set up to be sorted by
+/// the fragment offset, thus allowing easy final reassembly of the collected fragments.
 #[derive(Debug, Clone)]
-struct Piece {
+struct Fragment {
+    /// The bytes of the fragment
     message: Message,
+    /// The fragment offset of this fragment
     offset: u16,
 }
 
-impl Piece {
+impl Fragment {
+    /// Creates a new fragment
     pub fn new(message: Message, offset: u16) -> Self {
         Self { message, offset }
     }
 }
 
-impl PartialEq for Piece {
+impl PartialEq for Fragment {
     fn eq(&self, other: &Self) -> bool {
         self.offset == other.offset
     }
 }
 
-impl Eq for Piece {}
+impl Eq for Fragment {}
 
-impl PartialOrd for Piece {
+impl PartialOrd for Fragment {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.offset.partial_cmp(&other.offset)
     }
 }
 
-impl Ord for Piece {
+impl Ord for Fragment {
     fn cmp(&self, other: &Self) -> Ordering {
         self.offset.cmp(&other.offset)
     }
 }
 
+/// A vector of individual bits for space efficiency.
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct BitSet {
+struct BitVec {
     bits: Vec<u8>,
     len: u16,
 }
 
-impl BitSet {
+impl BitVec {
+    /// Creates a bit vector of the given length
     fn new(len: u16) -> Self {
         let bytes = (len - 1) / 8 + 1;
         Self {
@@ -214,14 +238,17 @@ impl BitSet {
         }
     }
 
+    /// Whether the given bit is set
     fn get(&self, bit: u16) -> bool {
         ((self.bits[bit as usize / 8] >> (bit % 8)) & 1) == 1
     }
 
+    /// Sets the given bit high
     fn set(&mut self, bit: u16) {
         self.bits[bit as usize / 8] |= 1 << (bit % 8);
     }
 
+    /// Sets the range of bits from start (inclusive) to end (exclusive) high.
     fn set_range(&mut self, start: u16, end: u16) {
         // TODO(hardint): Can be made more efficient by setting entire u8 blocks
         // instead of individual bits
@@ -230,6 +257,7 @@ impl BitSet {
         }
     }
 
+    /// Checks whether all the bits in the vector have been set high.
     fn complete(&self) -> bool {
         // TODO(hardint): Can be made more efficient by checking entire u8 blocks
         // instead of individual bits
@@ -243,7 +271,7 @@ mod tests {
 
     #[test]
     fn bit_set() {
-        let mut bits = BitSet::new(100);
+        let mut bits = BitVec::new(100);
         bits.set_range(10, 50);
         assert!(!bits.get(5));
         assert!(bits.get(10));
