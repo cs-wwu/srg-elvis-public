f6631098c39eb4bcbfef1d9940cd0ee0ec78e84c
sudobeans <skywd7766@gmail.com>
2023-10-08 12:13:10 -0700

    modify pci sessions to work like ipv4's kinda

--- sim/elvis-core/src/protocols/pci.rs
+++ sim/elvis-core/src/protocols/pci.rs
@@ -3,15 +3,35 @@
 use crate::{
     machine::{PciSlot, ProtocolMap},
     message::Message,
-    network::{Mac, Mtu},
+    network::{Delivery, Mac, Mtu},
     protocol::{DemuxError, StartError},
-    Control, Network, Protocol, Session, Shutdown,
+    Control, FxDashMap, Network, Protocol, Session, Shutdown,
 };
-use std::sync::Arc;
+use std::sync::{Arc, OnceLock};
 use tokio::sync::Barrier;
 
 pub mod pci_session;
 pub(crate) use pci_session::PciSession;
+
+/// A number used to identify protocols which communicate over Ethernet.
+///
+/// Examples:
+/// - IPv4 is identified as `0x0800`.
+/// - ARP is identified as `0x0806`.
+/// - IPv6 is identified as `0x086DD`.
+///
+/// See [`EtherTypes`] for an enum containing some of the popular types.
+///
+/// See <https://en.wikipedia.org/wiki/EtherType> for more information.
+pub type EtherType = u16;
+
+/// Some constants you can use to fill in an [`EtherType`].
+#[repr(u16)]
+pub enum EtherTypes {
+    Ipv4 = 0x0800,
+    Arp = 0x0806,
+    Ipv6 = 0x86DD,
+}
 
 /// Represents something akin to an Ethernet tap or a network interface card.
 ///
@@ -22,40 +42,151 @@ pub(crate) use pci_session::PciSession;
 /// u32 that specifies the `ProtocolId` of the protocol that should receive the
 /// message.
 pub struct Pci {
-    sessions: Vec<Arc<PciSession>>,
+    /// The protocols that have called .listen on this Pci.
+    listen_bindings: FxDashMap<EtherType, Arc<dyn Protocol>>,
+    /// Item #0 of the vec represents Pci Slot 0,
+    /// Item #1 of the vec represents Pci Slot 1, etc.
+    connections: Vec<Connection>,
+    protocols: OnceLock<ProtocolMap>,
 }
 
 impl Pci {
     /// Creates a new network tap.
     pub fn new(networks: impl IntoIterator<Item = Arc<Network>>) -> Self {
+        // Create a `Connection` object for each network
+        let networks = networks.into_iter();
+        let expected_size = networks.size_hint().1.unwrap_or(0);
+        let mut connections = Vec::with_capacity(expected_size);
+        for network in networks {
+            let conn = Connection {
+                mac: network.next_mac(),
+                network: network.clone(),
+            };
+            connections.push(conn);
+        }
+
         Self {
-            sessions: networks
-                .into_iter()
-                .enumerate()
-                .map(|(i, network)| PciSession::new(network, i as u32))
-                .collect(),
+            listen_bindings: FxDashMap::default(),
+            connections,
+            protocols: OnceLock::new(),
         }
     }
 
-    /// Gets the PCI session for the given slot.
+    /// Creates a new session.
+    ///
+    /// # Arguments
+    ///
+    /// * `slot`: the PCI slot (interface number)
+    /// that this machine will send messages out of.
+    /// For example, if you put the number 2,
+    /// this will send messages through the second network specified
+    /// when you created this PCI with [`Pci::new`].
+    ///
+    /// * `destination` - the destination MAC address.
+    ///
+    /// * `protocol` - the [`EtherType`] number of the destination protocol.
     ///
     /// # Panics
+    ///
     /// The user should ensure that the slot is less than [`Pci::slot_count`]
-    pub fn open(&self, slot: PciSlot) -> Arc<PciSession> {
-        self.sessions.get(slot as usize).unwrap().clone()
+    pub fn open(
+        &self,
+        slot: PciSlot,
+        destination: Option<Mac>,
+        protocol: EtherType,
+    ) -> Arc<PciSession> {
+        let connection = self.connections[slot as usize].clone();
+        let sesh = PciSession {
+            slot,
+            connection,
+            destination,
+            protocol,
+        };
+        Arc::new(sesh)
+    }
+
+    /// When Pci receives a message on the given Pci slot
+    /// and intended for the given EtherType
+    /// after calling this function,
+    /// it will send these messages to the upstream protocol
+    /// using `upstream.demux`.
+    ///
+    /// On success, returns `true`.
+    /// If a protocol had already been bound to the given `ethertype`,
+    /// this returns `false` and does not perform any binding.
+    pub fn listen(&self, upstream: Arc<dyn Protocol>, ethertype: EtherType) -> bool {
+        use dashmap::mapref::entry::Entry;
+        match self.listen_bindings.entry(ethertype) {
+            Entry::Occupied(_) => false,
+            Entry::Vacant(entry) => {
+                entry.insert(upstream);
+                true
+            }
+        }
     }
 
     pub fn slot_count(&self) -> usize {
-        self.sessions.len()
+        self.connections.len()
     }
 
     pub fn mac_addresses(&self) -> impl Iterator<Item = Mac> + '_ {
-        self.sessions.iter().map(|session| session.mac())
+        self.connections.iter().map(|connection| connection.mac)
     }
 
     /// Creates a new network tap.
     pub fn shared(self) -> Arc<Self> {
         Arc::new(self)
+    }
+
+    /// Called by the [`Network`] to pass a frame from the network up the
+    /// protocol stack. We use this instead of [`Protocol::demux`] because the
+    /// tap holds a reference to this session as a concrete type and having
+    /// specialized arguments to pass a full network frame to this session is
+    /// useful.
+    pub(crate) fn receive(self: &Arc<Self>, slot: PciSlot, delivery: Delivery) {
+        // get upstream protocol
+        let protocol = match self.listen_bindings.get(&delivery.protocol) {
+            Some(protocol) => Arc::clone(&protocol),
+            None => {
+                tracing::error!(
+                    "Could not find a protocol for the protocol ID {0:?}",
+                    delivery.protocol
+                );
+                return;
+            }
+        };
+
+        // Create a `control` and put in DemuxInfo
+        // so upstream protocols have info about the message they received
+        let mut control = Control::new();
+        let pci_demux_info = DemuxInfo {
+            slot,
+            source: delivery.sender,
+            destination: delivery.destination,
+            mtu: self.connections[slot].network.mtu,
+        };
+        control.insert(pci_demux_info);
+
+        // make a session so demux can work
+        // Currently, this makes a new session each time a message is received.
+        // We'll have to think about efficiency on this one...
+        let session = PciSession {
+            slot,
+            connection: self.connections[slot].clone(),
+            destination: Some(delivery.sender),
+            protocol: delivery.protocol,
+        };
+        let session = Arc::new(session);
+
+        // send upstream
+        let pmap = self
+            .protocols
+            .get()
+            .expect("start should be called before receive");
+        let result = protocol.demux(delivery.message, session, control, pmap.clone());
+        if let Err(e) = result {
+            tracing::error!("Error demuxing upstream from PCI: {}", e);
+        }
     }
 }
 
@@ -77,8 +208,18 @@ impl Protocol for Pci {
         initialized: Arc<Barrier>,
         protocols: ProtocolMap,
     ) -> Result<(), StartError> {
-        for session in self.sessions.iter() {
-            session.start(protocols.clone());
+        let self_arc = protocols
+            .protocol::<Pci>()
+            .expect("Pci should be in its own ProtocolMap on start");
+
+        let _ = self.protocols.set(protocols);
+
+        // Tell network about self
+        for slot in 0..self.connections.len() {
+            let connection = &self.connections[slot];
+            connection
+                .network
+                .register_tap(connection.mac, self_arc.clone(), slot);
         }
         initialized.wait().await;
         Ok(())
@@ -94,4 +235,15 @@ pub struct DemuxInfo {
     /// The local MAC address the message was sent to or none to indicate a broadcast message
     pub destination: Option<Mac>,
     pub mtu: Mtu,
+}
+
+/// Represents something akin to a NIC.
+/// Each PCI slot has a connection.
+///
+/// * `mac` - the MAC address associated with this connection
+/// * `network` - the network this NIC is connected to.
+#[derive(Clone)]
+struct Connection {
+    pub mac: Mac,
+    pub network: Arc<Network>,
 }
--- sim/elvis-core/src/protocols/pci/pci_session.rs
+++ sim/elvis-core/src/protocols/pci/pci_session.rs
@@ -4,41 +4,27 @@ use crate::{
     network::{Delivery, Mac, Mtu},
     protocol::DemuxError,
     session::SendError,
-    Control, Network, Session,
+    Session,
 };
-use std::{
-    any::TypeId,
-    sync::{Arc, RwLock},
-};
+use std::any::TypeId;
 
-use super::DemuxInfo;
+use super::Connection;
 
 /// The session type associated with [`Pci`](super::Pci).
-/// Contains information about a connection between this MAC address and the network.
+/// Contains information about a connection between a PCI slot and the network.
 pub struct PciSession {
-    mac: Mac,
-    slot: PciSlot,
-    network: Arc<Network>,
-    protocols: RwLock<Option<ProtocolMap>>,
+    /// The local Pci slot associated with this connection.
+    pub(super) slot: PciSlot,
+    /// The connection to the network.
+    pub(super) connection: Connection,
+    /// The destination MAC address.
+    pub(super) destination: Option<Mac>,
+    /// The EtherType of the destination protocol.
+    pub(super) protocol: super::EtherType,
 }
-
 impl PciSession {
-    /// Creates a new Tap session
-    pub(super) fn new(network: Arc<Network>, index: u32) -> Arc<Self> {
-        let mac = network.next_mac();
-        let this = Self {
-            mac,
-            slot: index,
-            network: network.clone(),
-            protocols: Default::default(),
-        };
-        let this = Arc::new(this);
-        network.register_tap(mac, this.clone());
-        this
-    }
-
     pub fn mac(&self) -> Mac {
-        self.mac
+        self.connection.mac
     }
 
     pub fn slot(&self) -> PciSlot {
@@ -46,74 +32,35 @@ impl PciSession {
     }
 
     pub fn mtu(&self) -> Mtu {
-        self.network.mtu
+        self.connection.network.mtu
     }
 
-    /// Called by the owning [`Pci`] protocol at the beginning of the simulation
-    /// to start the contained tap running
-    pub(super) fn start(&self, protocols: ProtocolMap) {
-        *self.protocols.write().unwrap() = Some(protocols);
-    }
-
-    /// Called by the owned [`Tap`] to pass a frame from the network up the
-    /// protocol stack. We use this instead of [`Session::receive`] because the
-    /// tap holds a reference to this session as a concrete type and having
-    /// specialized arguments to pass a full network frame to this session is
-    /// useful.
-    pub(crate) fn receive(self: &Arc<Self>, delivery: Delivery) -> Result<(), ReceiveError> {
-        let mut control = Control::new();
-        let pci_demux_info = DemuxInfo {
-            slot: self.slot,
-            source: delivery.sender,
-            destination: delivery.destination,
-            mtu: self.network.mtu,
-        };
-        control.insert(pci_demux_info);
-        let protocols = self.protocols.read().unwrap().as_ref().unwrap().clone();
-        let protocol = match protocols.get(delivery.protocol) {
-            Some(protocol) => protocol,
-            None => {
-                tracing::error!(
-                    "Could not find a protocol for the protocol ID {0:?}",
-                    delivery.protocol
-                );
-                Err(ReceiveError::Protocol(delivery.protocol))?
-            }
-        };
-
-        protocol.demux(delivery.message, self.clone(), control, protocols)?;
-        Ok(())
-    }
-
-    pub fn send_pci(
-        &self,
-        message: Message,
-        remote_mac: Option<Mac>,
-        receiver: TypeId,
-    ) -> Result<(), SendError> {
-        if message.len() > self.network.mtu as usize {
-            return Err(SendError::Mtu(self.network.mtu));
+    /// Similar to [`PciSession::send`],
+    /// but does not require a ProtocolMap.
+    pub fn send_pci(&self, message: Message) -> Result<(), SendError> {
+        if message.len() > self.connection.network.mtu as usize {
+            return Err(SendError::Mtu(self.connection.network.mtu));
         }
 
         let delivery = Delivery {
             message,
-            sender: self.mac,
-            destination: remote_mac,
-            protocol: receiver,
+            sender: self.connection.mac,
+            destination: self.destination,
+            protocol: self.protocol,
         };
 
-        let network = self.network.clone();
+        let network = self.connection.network.clone();
         tokio::spawn(async move {
             network.send(delivery).await;
         });
+
         Ok(())
     }
 }
 
-// TODO(hardint): I think PCI sessions maybe just shouldn't implement to session type
 impl Session for PciSession {
-    fn send(&self, _message: Message, _protocols: ProtocolMap) -> Result<(), SendError> {
-        panic!("Should use PciSession::send_pci instead");
+    fn send(&self, message: Message, _protocols: ProtocolMap) -> Result<(), SendError> {
+        self.send_pci(message)
     }
 }
 
