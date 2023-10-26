9a1996ef64216d0caabdd17ffdb396a1c616a98c
Zach <deshawz@wwu.edu>
2023-10-26 14:56:43 -0700

    terminal_echo sim complete!

--- sim/elvis/src/simulations/terminal_echo.rs
+++ sim/elvis/src/simulations/terminal_echo.rs
@@ -1,13 +1,8 @@
-use crate::applications::{Capture, SendMessage, Terminal};
+use crate::applications::Terminal;
 use elvis_core::{
-    message::Message,
     new_machine,
-    protocols::{
-        ipv4::{Ipv4, Ipv4Address, Recipient},
-        udp::Udp,
-        Endpoint, Pci,
-    },
-    run_internet, ExitStatus, IpTable, Network,
+    protocols::udp::Udp,
+    run_internet, ExitStatus,
 };
 
 /// Runs a basic simulation.
@@ -17,25 +12,8 @@ use elvis_core::{
 pub async fn terminal_echo() {
     println!("Begin test");
 
-    let network = Network::basic();
-    let message = Message::new("Hello!");
-    let endpoint = Endpoint {
-        address: [123, 45, 67, 89].into(),
-        port: 0xbeef,
-    };
-
-    let local_address: Ipv4Address = [127, 0, 0, 1].into();
-
-    let ip_table: IpTable<Recipient> = [(local_address, Recipient::with_mac(0, 1))]
-        .into_iter()
-        .collect();
-
     let machines = vec![
         new_machine![
-            Udp::new(),
-            Ipv4::new(ip_table.clone()),
-            Pci::new([network.clone()]),
-            SendMessage::new(vec![message.clone()], endpoint),
             Udp::new(),
             Terminal::new(String::from("localhost:8080")),
         ],
