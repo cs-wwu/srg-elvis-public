b4f3251c78ffabc0c85cb4f18c2904d91d6f967b
giddinl2 <giddinl2@wwu.edu>
2023-11-02 18:35:15 -0700

    Reverting main.rs back to normal

--- sim/elvis/src/main.rs
+++ sim/elvis/src/main.rs
@@ -1,13 +1,10 @@
-use elvis::simulations::socket_basic;
-use elvis_core::protocols::socket_api::socket::SocketType;
+use elvis::cli::initialize_from_arguments;
 use std::env;
 
 /// Without arguments, will do nothing
 #[tokio::main]
 pub async fn main() {
     println!("Elvis v{}", env!("CARGO_PKG_VERSION"));
-    //initialize_from_arguments().await;
-    socket_basic(SocketType::Datagram, 255, false, 0).await;
-    socket_basic(SocketType::Stream, 255, false, 0).await;
+    initialize_from_arguments().await;
     println!("Done");
 }
