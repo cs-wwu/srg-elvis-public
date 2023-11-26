7a294e287191c7c6a8625520ed3f1a166cc7c448
sudobeans <skywd7766@gmail.com>
2023-11-26 15:04:12 -0800

    remove passive shutdown

--- sim/elvis-core/src/shutdown.rs
+++ sim/elvis-core/src/shutdown.rs
@@ -1,38 +1,60 @@
-use tokio::sync::{broadcast, mpsc};
+use tokio::sync::broadcast;
 
+/// A struct which can be used to shut down a simulation.
+/// You can create multiple connected shutdowns by cloning.
 #[derive(Debug, Clone)]
 pub struct Shutdown {
-    /// This channel can be used tell the simulation to shutdown or wait for the
-    /// simulation shutdown message
+    /// This channel can be used tell the simulation to shut down.
     notify: broadcast::Sender<ExitStatus>,
-
-    /// The internet will wait for all Shutdown instances to be dropped using
-    /// this channel.
-    #[allow(dead_code)]
-    confirm: mpsc::Sender<ExitStatus>,
+    /// Keeps track of the last status received
+    /// So users can call `wait_for_shutdown` multiple times
+    last_status: Option<ExitStatus>,
 }
 
 impl Shutdown {
+    /// Creates a new active shutdown.
     pub fn new() -> Self {
         let (notify, _) = broadcast::channel(1);
-        let (confirm, _) = mpsc::channel(1);
-        Self { notify, confirm }
+        Self {
+            notify,
+            last_status: None,
+        }
     }
 
+    /// Sends `ExitStatus::Exited` to all `Shutdowns` cloned from this one.
     pub fn shut_down(&self) {
         if let Err(e) = self.notify.send(ExitStatus::Exited) {
             tracing::error!("Failed to initiate shutdown: {}", e);
         }
     }
 
+    /// Sends `status` to all `Shutdowns` cloned from this one.
     pub fn shut_down_with_status(&self, status: ExitStatus) {
         if let Err(e) = self.notify.send(status.clone()) {
             tracing::error!("Failed to initiate shutdown: {}", e);
         }
     }
 
-    pub fn receiver(&self) -> broadcast::Receiver<ExitStatus> {
-        self.notify.subscribe()
+    /// Waits to receive a shutdown status.
+    pub async fn wait_for_shutdown(&mut self) -> ExitStatus {
+        use tokio::sync::broadcast::error::RecvError;
+
+        if let Some(status) = self.last_status {
+            return status;
+        } else {
+            let mut recv = self.notify.subscribe();
+        
+            loop {
+                match recv.recv().await {
+                    Ok(status) => {
+                        self.last_status = Some(status);
+                        return status;
+                    },
+                    Err(RecvError::Closed) => unreachable!(),
+                    Err(RecvError::Lagged(_)) => (),
+                }
+            };
+        }
     }
 }
 
@@ -42,9 +64,27 @@ impl Default for Shutdown {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum ExitStatus {
     Status(u32),
     Exited,
     TimedOut,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_active() {
+        let status = ExitStatus::Status(22);
+        let shut0 = Shutdown::new();
+        let mut shuts = [shut0.clone(), shut0.clone(), shut0.clone()];
+
+        shuts[0].shut_down_with_status(status);
+
+        for shut in shuts {
+            assert_eq!(shut.wait_for_shutdown().await, status);
+        }
+    }
 }
