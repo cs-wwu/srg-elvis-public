17e6bef9b18d1596ed3ace47fee9c1e02cdacf83
logannday <logannday@gmail.com>
2024-01-11 13:48:15 -0800

    Added usage guide to terminal_receive

--- sim/elvis/src/simulations/terminal_receive.rs
+++ sim/elvis/src/simulations/terminal_receive.rs
@@ -9,14 +9,35 @@ use elvis_core::{
     ExitStatus, IpTable, Network, run_internet,
 };
 
-/// In this simulation, 
+/*
+* Usage:
+*               # Start the sim #
+*
+*   $cargo test terminal_receive -- --nocapture
+*       output: Begin run on port 127.0.0.1:[portnumber]
+*               Begin run on port 127.0.0.1:[other portnumber]
+*
+*         # Open two new terminals and connect to machines #
+*
+*   $telnet localhost [portnumber]
+*   $telnet localhost [other portnumber]
+*       output: Connected to localhost.
+*               Escape character is '^]'.
+*
+*   # Send message between terminals #
+*
+*   $send [endpoint] message 
+*       note: [endpoints] are commented next to the terminal receive params
+*/
 pub async fn terminal_receive() {
     let network = Network::basic();
     let endpoint = Endpoint {
+        // endpoint: 123.45.67.89:48879
         address: [123, 45, 67, 89].into(),
         port: 0xbeef, // 48879
     };
     let local = Endpoint {
+        // endpoint: 123.44.66.88:65261
         address: [123, 44, 66, 88].into(),
         port: 0xfeed, // 65261
     };
