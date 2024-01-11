8bfd8146e4c91d0c0370915ff490f3804c944902
kailahulse <kailahulse@gmail.com>
2024-01-11 12:13:59 -0800

    Added user and tcpStream to sim

--- sim/elvis/src/simulations/complex_sim.rs
+++ sim/elvis/src/simulations/complex_sim.rs
@@ -27,11 +27,14 @@ pub async fn complex_sim() {
     let network = Network::basic();
     let user_server_ip_address: Ipv4Address = [100, 42, 0, 0].into(); // I know this one is necessary for user functionality right now
     let socket_server_ip_address: Ipv4Address = [111, 111, 11, 0].into();
+    let localhost_ip_address: Ipv4Address = [123, 45, 67, 89].into();
+    let client_ip_address: Ipv4Address = [123, 45, 67, 90].into();
+    let server_socket_address: Endpoint = Endpoint::new(localhost_ip_address, 80);
+    let server_socket_addressu: Endpoint = Endpoint::new(user_server_ip_address, 80);
+    let client_socket_address: Endpoint = Endpoint::new(client_ip_address, 70);
     let dns_server_ip_address = Ipv4Address::DNS_AUTH;
     let dns_again_server_ip_address: Ipv4Address = [123, 45, 67, 15].into();
     let dhcp_server_ip_address: Ipv4Address = [123, 123, 123, 123].into(); //diff from their sim
-    let localhost_ip_address: Ipv4Address = [123, 45, 67, 89].into();
-    let userclient_ip_address: Ipv4Address = [123, 45, 67, 90].into();
     // look at how server_experiment generates ip_addresses 
     // This is from all the other sims initial IpTable I believe
     let ip_table: IpTable<Recipient> = [("0.0.0.0/0", Recipient::new(0, None))]
@@ -54,29 +57,44 @@ pub async fn complex_sim() {
             SocketAPI::new(Some(user_server_ip_address)),
             WebServer::new(WebServerType::Yahoo, None),
         ],
+        new_machine_arc![
+            Tcp::new(),
+            Ipv4::new(ip_table.clone()),
+            Pci::new([network.clone()]),
+            Arp::new(),
+            SocketAPI::new(Some(client_ip_address)),
+            UserBehavior::new(server_socket_address),
+        ],
     ];
+    let machines = vec![
+        new_machine_arc![
+            Tcp::new(),
+            Ipv4::new(ip_table.clone()),
+            Pci::new([network.clone()]),
+            Arp::new(),
+            SocketAPI::new(Some(localhost_ip_address)),
+            TcpStreamClient::new(server_socket_address, client_socket_address),
+        ],
+        new_machine_arc![
+            Tcp::new(),
+            Ipv4::new(ip_table.clone()),
+            Pci::new([network.clone()]),
+            Arp::new(),
+            SocketAPI::new(Some(client_ip_address)),
+            TcpListenerServer::new(server_socket_address, client_socket_address),
+        ],
+    ];
+    // Socket basic
 
-    // v2 for combo
-    // let machines = vec![
-    //     new_machine_arc![
-    //         Udp::new(),
-    //         Tcp::new(),
-    //         Ipv4::new(ip_table.clone()),
-    //         Pci::new([network.clone()]),
-    //         Arp::new(),
-    //         SocketAPI::new(Some(user_server_ip_address)),
-    //         WebServer::new(WebServerType::Yahoo, None),
-    //         DnsTestServer::new(0xbeef, SocketType::Datagram)
-    //         ...
-    //     ],
-    // ];
+    
     let status = run_internet_with_timeout(&machines, Duration::from_secs(2)).await;
     assert_eq!(status, ExitStatus::Exited);
     // for testing afterwards
     let mut machines_iter = machines.into_iter();
     let _server = machines_iter.next().unwrap();
+}
 
-    #[cfg(test)]
+#[cfg(test)]
 mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn complex_sim() {
@@ -86,4 +104,3 @@ mod tests {
     }
 }
 
-}
